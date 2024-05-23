ba62e1d3d92dff501f7494fe6e5acd541386ec02
osaiz <osaiz@todosistemassti.co>
2024-05-22 21:40:16 -0500

    Menu Inicial

---
+++ src/Login.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+
+const Login = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div >
+      <div className="login-box">
+        <div className="login-logo">
+          <a><b>I</b>nventario</a>
+        </div>
+        <div className="login-box-body">
+          <p className="login-box-msg">Sign in to start your session</p>
+
+          <form action="" method="post">
+            <div className="form-group has-feedback">
+              <input type="email" className="form-control" placeholder="Email" />
+              <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div className="form-group has-feedback">
+              <input type="password" className="form-control" placeholder="Password" />
+              <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div className="row">
+              <div className="col-xs-12">
+                <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
+              </div>
+            </div>
+          </form>
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
